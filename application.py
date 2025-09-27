97d02f9910d01e0b8f1029a8f60b30a39d74871b
Andy5614 <136292514+Andy5614@users.noreply.github.com>
2025-09-27 19:33:24 +0800

    Create application.py

---
+++ application.py
@@ -0,0 +1,13 @@
+# application.py
+from flask import Flask
+
+# Elastic Beanstalk 預設會尋找名為 'application' 的 WSGI 變數
+application = Flask(__name__)
+
+@application.route('/')
+def index():
+    return "Hello, Elastic Beanstalk! 部署成功 🎉"
+
+# 本地測試時可以使用這段
+if __name__ == "__main__":
+    application.run(host='0.0.0.0', port=5000)
